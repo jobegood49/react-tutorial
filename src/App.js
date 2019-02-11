044bd2efa7aab55155c093870524c143adafc947
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-11 19:14:14 +0700

    Add add task functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react'
 import './App.css'
 
 class TaskForm extends React.Component {
+  state = {
+    inputValue: '',
+  }
+  addTask = event => {
+    event.preventDefault()
+    this.props.addTask(this.state.inputValue)
+  }
+
+  handleChange = event => {
+    this.setState({ inputValue: event.target.value })
+  }
+
   render() {
     return (
       <div>
-        <form className="task">
-          <input type="text" placeholder="add a task" />
+        <form onSubmit={this.addTask} className="task">
+          <input
+            type="text"
+            placeholder="add a task"
+            onChange={this.handleChange}
+          />
           <input type="submit" value="Add" />
         </form>
       </div>
@@ -15,30 +31,10 @@ class TaskForm extends React.Component {
 }
 
 class TaskList extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      tasks: [
-        {
-          id: 1,
-          text: 'Learn HTLM',
-        },
-        {
-          id: 2,
-          text: 'Learn React',
-        },
-        {
-          id: 3,
-          text: 'Learn Vue',
-        },
-      ],
-    }
-  }
-
   render() {
     return (
       <div>
-        {this.state.tasks.map(task => {
+        {this.props.tasks.map(task => {
           return <Task key={task.id} text={task.text} />
         })}
       </div>
@@ -52,11 +48,40 @@ class Task extends React.Component {
   }
 }
 class App extends Component {
+  state = {
+    tasks: [
+      {
+        id: 1,
+        text: 'Learn HTLM',
+      },
+      {
+        id: 2,
+        text: 'Learn React',
+      },
+      {
+        id: 3,
+        text: 'Learn Vue',
+      },
+    ],
+  }
+
+  addTask = value => {
+    console.log('add task')
+    let newTask = {
+      id: 4,
+      text: value,
+    }
+
+    this.setState({
+      tasks: [...this.state.tasks, newTask],
+    })
+  }
+
   render() {
     return (
       <div>
-        <TaskForm />
-        <TaskList />
+        <TaskForm addTask={this.addTask} />
+        <TaskList tasks={this.state.tasks} />
       </div>
     )
   }
