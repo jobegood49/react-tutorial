3d099a9e39bec4b98b432534e3564465313ad380
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-11 16:50:51 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
+class TaskForm extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form className="task">
+          <input type="text" placeholder="add a task" />
+          <input type="submit" value="Add" />
+        </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class TaskList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      tasks: [
+        {
+          id: 1,
+          text: 'Learn HTLM',
+        },
+        {
+          id: 2,
+          text: 'Learn React',
+        },
+        {
+          id: 3,
+          text: 'Learn Vue',
+        },
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.tasks.map(task => {
+          return <Task id={task.id} text={task.text} />
+        })}
+      </div>
+    )
+  }
+}
+
+class Task extends React.Component {
+  render() {
+    return <div key={this.props.id}>{this.props.text}</div>
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TaskForm />
+        <TaskList />
+      </div>
+    )
+  }
+}
+
+export default App
